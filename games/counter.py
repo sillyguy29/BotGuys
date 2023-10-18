c2ce3f6f1bcb09a5441cf9da3473de3ea759fc64
Sam C <samcashman361@gmail.com>
2023-10-18 15:45:51 -0400

    Added documentation, bugfixes, QoL improvements

--- games/counter.py
+++ games/counter.py
@@ -5,66 +5,105 @@ from games.game import GameManager
 
 class CounterGame(BaseGame):
     def __init__(self):
+        # game_type = 0 -> counter game
         super().__init__(game_type=0)
+        # data specific to this game (counter) defined here
         self.count = 0
 
 
-# TODO: Handle cases where a user closes a game and another user attempts
-# to interact with the closed game, might cause issues
 class CounterManager(GameManager):
+    """
+    Manages a counter game. Has operations to increment and decrement the count
+    """
     def __init__(self, factory, channel_id):
         super().__init__(CounterGame(), CounterButtonsBase(self), channel_id, factory, None)
 
     def get_base_menu_string(self):
         return "Counter: {}".format(self.game.count)
     
-    async def increment(self):
+    async def increment(self, interaction):
+        # check to make sure the game hasn't ended, do nothing if it has
+        if await self.game_end_check(interaction):
+            return
+        
         self.game.count += 1
+        # send a secret message to the person who pressed this button. This message auto-deletes
+        # after 10 seconds (doesn't need to be around for much longer)
+        await interaction.response.send_message("You've hit it!", ephemeral = True,
+                                                delete_after = 10)
+        # resend the base menu with the updated game state
         await self.resend()
 
-    async def decrement(self):
+    async def decrement(self, interaction):
+        # check to make sure the game hasn't ended, do nothing if it has
+        if await self.game_end_check(interaction):
+            return
+        
         self.game.count -= 1
+        # send a secret message to the person who pressed this button. This message auto-deletes
+        # after 10 seconds (doesn't need to be around for much longer)
+        await interaction.response.send_message("You've missed it!", ephemeral = True,
+                                                delete_after = 10)
+        # resend the base menu with the updated game state
         await self.resend()
 
 
 class HitOrMiss(discord.ui.View):
+    """
+    Button group used on the private message that users use to increment and decrement
+    the counter for the game.
+    """
     def __init__(self, manager):
         super().__init__()
         self.manager = manager
 
     @discord.ui.button(label = "Hit Me!", style = discord.ButtonStyle.green)
     async def hit_me(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # stop accepting interactions for this message
         self.stop()
-        await self.manager.increment()
-        await interaction.response.send_message("You've hit it!", ephemeral = True)
+        await self.manager.increment(interaction)
 
     @discord.ui.button(label = "Miss Me!", style = discord.ButtonStyle.green)
     async def miss_me(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # stop accepting interactions for this message
         self.stop()
-        await self.manager.decrement()
-        await interaction.response.send_message("You've missed it!", ephemeral = True)
+        await self.manager.decrement(interaction)
     
 
 class CounterButtonsBase(discord.ui.View):
+    """
+    Base menu button group for the counter game.
+    """
     def __init__(self, manager):
         super().__init__()
         self.manager = manager
     
     @discord.ui.button(label = "Hit or Miss", style = discord.ButtonStyle.green)
     async def hit_miss(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # send a private message to the person who pressed this button with the hit or miss view,
+        # it auto deletes after 60 seconds (prevents dangling messages that can force the game to 
+        # stay in memory after it ends)
         view = HitOrMiss(self.manager)
-        await interaction.response.send_message("Hit or Miss?", view = view, ephemeral = True)
+        await interaction.response.send_message("Hit or Miss?", view = view, ephemeral = True,
+                                                delete_after = 60)
+        # wait for the view to call self.stop() before we move beyond this point
         await view.wait()
+        # delete the response to the hit or miss button press (the HitOrMiss UI message)
         await interaction.delete_original_response()
 
     @discord.ui.button(label = "Refresh", style = discord.ButtonStyle.blurple)
     async def ref(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.manager.refresh()
-        await interaction.response.send_message("Refreshing the counter...", ephemeral = True)
+        # get the manager of this view to refresh the base menu message
+        await self.manager.refresh(interaction)
+        # send a secret message to the person who pressed this button. This message auto-deletes
+        # after 10 seconds (doesn't need to be around for much longer)
+        await interaction.response.send_message("Refreshing the counter...", ephemeral = True,
+                                                delete_after = 10)
 
     @discord.ui.button(label = "Quit", style = discord.ButtonStyle.red)
     async def quit(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.manager.quit_game()
+        # as our manager to quit this game
+        await self.manager.quit_game(interaction)
 
 
 
--- games/game.py
+++ games/game.py
@@ -1,53 +1,124 @@
 import discord
 import games.counter
 
-# Basic game class. No reason not to reference member vars directly
 class BaseGame():
+    """
+    Game model class. Member vars should only be accessed by its manager or AI functions.
+    """
     def __init__(self, game_type=0, number_of_players=0, max_players=0, 
                  player_list=None, game_state=0, channel_id=None):
+        # ID value of the game type
         self.game_type = game_type
         self.number_of_players = number_of_players
+        # list of players engaged with this game
         self.player_list = player_list
+        # current game state, used to help manage certain outside interactions
+        # game_state = 0 -> game is open to anyone at any time
         self.game_state = game_state
         self.max_players = max_players
 
 
-# Base class for a game. Most of the methods should be overriden
 class GameManager():
+    """
+    Manages the most basic interactions for any game, such as creation, base menu
+    management, etc. Should be subclassed by each game type's manager class.
+    Methods can (and should) be overridden but be careful when doing so as to not
+    break the default flow of all games
+    """
     def __init__(self, game, base_gui, channel_id, factory, current_active_menu):
+        # hold the game model that this manager needs to manage (pass constructor to 
+        # subclass of BaseGame for that game)
         self.game = game
+        # default button layout that the bot can use to construct the base menu at any time
         self.base_gui = base_gui
+        # ID of the channel that this game is taking place in
         self.channel_id = channel_id
+        # reference to the GameFactory class, needed to remove the game from the active games
+        # dict upon the game ending
         self.factory = factory
+        # reference to the message that currently contains the base menu. Needed so that the
+        # bot can remove the buttons from it or edit its contents at any time
         self.current_active_menu = current_active_menu
 
     async def create_game(self, interaction):
+        """
+        Send the base menu for the first time. The interaction passed in should be
+        the slash command that started the game
+        """
+        # construct the first base menu message, grabbing the buttons from self.base_gui
+        # and the message contents from self.get_base_menu_string
         await interaction.response.send_message(content=self.get_base_menu_string(), 
                                                 view=self.base_gui)
+        # set our base menu message to the message that the interaction (ie the slash command
+        # that started the game) was responded with (the base menu created by this interaction)
         self.current_active_menu = await interaction.original_response()
 
-    async def refresh(self):
+    async def refresh(self, interaction):
+        """
+        Edit the active base menu to reflect the current game state
+        """
+        # check to make sure the game hasn't ended, do nothing if it has
+        if await self.game_end_check(interaction):
+            return
+        
         await self.current_active_menu.edit(content=self.get_base_menu_string(), 
                                             view=self.base_gui)
 
-    async def resend(self):
+    async def resend(self, interaction):
+        """
+        Remove the buttons from the current active base menu and send a new one
+        """
+        # check to make sure the game hasn't ended, do nothing if it has
+        if await self.game_end_check(interaction):
+            return
+        
+        # removes the view (which contains the buttons) from the current active base menu
         await self.current_active_menu.edit(view=None)
-        # InteractionMessage inherits from Message so we can access the channel attribute
+        # InteractionMessage inherits from Message so we can access the channel attribute to
+        # send a new base menu into
         self.current_active_menu = await self.current_active_menu.channel.send(
                                         self.get_base_menu_string(), view=self.base_gui)
         
-    async def quit_game(self):
+    async def quit_game(self, interaction):
+        """
+        Stop the game by removing the buttons from its current active base menu and
+        asking the game factory to remove it from the dictionary. Set its game state
+        to -1, meaning the game is over
+        """
+        # check to make sure we haven't already ended, do nothing if we have
+        if await self.game_end_check(interaction):
+            return
+        
+        self.game.game_state = -1
         await self.current_active_menu.edit(view=None)
         await self.factory.stop_game(self.channel_id)
 
     def get_base_menu_string(self):
+        """
+        Return a string that represents the current state of the game, as shown to
+        ALL players. This method must be overridden
+        """
+        # might be a good idea to replace this with an exception
         return "Generic game menu message"
+    
+    async def game_end_check(self, interaction):
+        # if the game has ended, notify the user and don't do anything
+        if self.game.game_state == -1:
+            await interaction.response.send_message("This game has ended.", 
+                                                    ephemeral = True,
+                                                    delete_after = 10)
+            # True -> game has ended
+            return True
+        # False -> game has not ended
+        return False
 
 
-# General game factory class, handles interactions between the client and the game presenters,
-# manages all active games. This class should not use any mutator methods in the manager classes
-# except for create_game.
 class GameFactory():
+    """
+    General game factory class, handles interactions between the client and the game presenters,
+    manages all active games. This class should not use any mutator methods in the manager classes
+    except for create_game.
+    """
     def __init__(self):
         self.active_games = dict()
 
