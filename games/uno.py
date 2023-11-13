fdad9e8bec078f102fc69dcbe846213d86418292
MattThunder <matteo_dejesus12@hotmail.com>
2023-11-13 17:08:05 -0500

    Made Card class more general, added to uno.py

--- games/uno.py
+++ games/uno.py
@@ -8,7 +8,7 @@ import discord
 from games.game import BaseGame
 from games.game import GameManager
 from util import Card
-from util import generate_deck_Uno
+#from util import generate_deck_Uno
 import random
 
 
@@ -22,6 +22,9 @@ class UnoGame(BaseGame):
         
         self.deck = generate_deck_Uno()
         random.shuffle(self.deck)
+        self.discard = []
+        self.players = []
+        self.top_card = UnoCard("None", "")
       
         
 class UnoManager(GameManager):
@@ -31,6 +34,8 @@ class UnoManager(GameManager):
         
         # What is player_list? We have player_data (dict) in the game model
         #self.game.player_list.append(user_id)
+        
+        self.setup()
 
     #async def create_game(self, interaction):
     #    raise NotImplementedError("Override this method based on uno specifications")
@@ -45,6 +50,43 @@ class UnoManager(GameManager):
             return "Welcome to this game of Uno. Feel free to join."
         elif self.game.game_state == 4:
             return "Game has started!"
+        
+    def setup(self):
+        for i in range(1, 6):
+            self.game.players.append(Player(f"Player {i}"))
+        
+        # Draw 7 cards for each player
+        for player in self.game.players:
+            self.draw_cards(player,7)
+
+        # Assign the top-card. The game cannot begin on a "Reverse", "Skip", "Draw Two", or "Wild"
+        print()
+        print("Setting up the table...")
+        while True:
+            self.game.top_card = self.game.deck.pop()
+            print(f"The top card is {self.game.top_card}. ", end="")
+            top_card_is_valid = True
+            if (self.game.top_card.value == "Reverse"): top_card_is_valid = False
+            if (self.game.top_card.value == "Skip"): top_card_is_valid = False
+            if (self.game.top_card.value == "Draw Two"): top_card_is_valid = False
+            if (self.game.top_card.name == "Wild"): top_card_is_valid = False
+            if not top_card_is_valid: 
+                print("That's no good... Picking a new top card...")
+            else:
+                print("Let the game begin!")
+                break
+            
+    def draw_cards(self, player, num_cards=1):
+        for i in range(num_cards):
+            if len(self.game.deck) == 0: 
+                self.announce("The deck is empty! Regenerating deck...")
+                self.regenerateDeck()
+            card = self.game.deck.pop()
+            player.hand.append(card)
+            player.hand = sorted(player.hand)
+    
+    def get_player_hand(self, index):
+        return self.game.players[index].hand
 
   
 class CardButton(discord.ui.Button):
@@ -80,7 +122,7 @@ class UnoCardButtons(discord.ui.View):
         
         for i in range(self.cards_num):
             # example of what can be done
-            self.add_item(CardButton(self.manager, f"Green {i}"))
+            self.add_item(CardButton(self.manager, self.manager.get_player_hand(0)[i]))
  
         
 class UnoButtonsBase(discord.ui.View):
@@ -101,7 +143,7 @@ class UnoButtonsBase(discord.ui.View):
         game end).
         """
         print(f"{interaction.user} pressed {button.label}!")
-        view = UnoCardButtons(self.manager, 3)
+        view = UnoCardButtons(self.manager, len(self.manager.get_player_hand(0)))
         await interaction.response.send_message("Your cards:", view = view, ephemeral = True,
                                                 delete_after = 60)
         # wait for the view to call self.stop() before we move beyond this point
@@ -124,3 +166,61 @@ class UnoButtonsBase(discord.ui.View):
         #    await self.manager.quit_game(interaction)
         await self.manager.quit_game(interaction)
 
+
+class UnoCard(Card):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+        
+        self.priority = 0
+        if self.name == "Red": self.priority += 0
+        if self.name == "Blue": self.priority += 15
+        if self.name == "Green": self.priority += 30
+        if self.name == "Yellow": self.priority += 45
+        if self.name == "Wild": self.priority += 60
+
+        if self.value == "Reverse": self.priority += 11
+        elif self.value == "Skip": self.priority += 12
+        elif self.value == "Draw Two": self.priority += 13
+        elif self.value == "Card": self.priority += 1
+        elif self.value == "Draw Four": self.priority += 2
+        elif self.value == "": self.priority += 0
+        else: self.priority += int(self.value)
+    
+    def __eq__(self, other):
+        if not isinstance(other, Card): return False
+        if self.name != other.name: return False
+        if self.value != other.value: return False
+        return True
+    
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        return self.priority > other.priority
+    
+    
+def generate_deck_Uno():
+    deck = []
+    for color in ('Red', 'Yellow', 'Green', 'Blue'):
+        deck.append(UnoCard(color, '0'))
+        for value in range(1,10):
+            deck.append(UnoCard(color, str(value)))
+            deck.append(UnoCard(color, str(value)))
+        for value in range(2):
+            deck.append(UnoCard(color, "Draw Two"))
+            deck.append(UnoCard(color, "Reverse"))
+            deck.append(UnoCard(color, "Skip"))
+    for value in range(4):
+        deck.append(UnoCard("Wild", ""))
+        deck.append(UnoCard("Wild", "Draw Four"))
+    return deck
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        self.skipped = False
+        
+    def get_playable_cards(self, top_card):
+        playable_cards = [card for card in self.hand if card.color == top_card.color or card.value == top_card.value or card.color == "Wild"]
+        return playable_cards
--- util.py
+++ util.py
@@ -4,22 +4,22 @@ class Card:
     This data type represents the playing cards found in a standard
     52-card deck.
     """
-    def __init__(self, suit, face):
+    def __init__(self, name, value):
         """
-        suit is H, D, S, C, for Heart, Diamond, Spade, Club
-        face is 2-10, J, Q, K, A, for the numbered and real face cards
+        name (suit) is H, D, S, C, for Heart, Diamond, Spade, Club
+        value (face) is 2-10, J, Q, K, A, for the numbered and real face cards
         value refers to the numerical value as used in Blackjack
             Ace cards should have value set to 1
         """
-        self.suit = suit
-        self.face = face
+        self.name = name
+        self.value = value
 
     def __eq__(self, other):
-        return self.suit == other.suit\
-                and self.face == other.face\
+        return self.name == other.name\
+                and self.value == other.value\
     
     def __str__(self):
-        return str(self.suit) + str(self.face)
+        return f"{self.name} {self.value}"
 
 
 def generate_deck():
@@ -32,7 +32,7 @@ def generate_deck():
             deck.append(Card(suit, face))       
     return deck
  
-            
+"""
 def generate_deck_Uno():
     deck = []
     for color in ('Red', 'Yellow', 'Green', 'Blue'):
@@ -48,3 +48,4 @@ def generate_deck_Uno():
         deck.append(Card("Wild", ""))
         deck.append(Card("Wild", "+4"))
     return deck
+"""
